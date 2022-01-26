fe3b050dbb7b375a60c4da3fe550eccb4cad088f
brendanou <0132735@kdu-online.com>
2022-01-26 14:41:12 +0800

    Update PlayerController.cs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        speed2 = 1;
+        speed2 = 11;
     }
 
     // Update is called once per frame
