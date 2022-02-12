59005e19fa155800c5a40f1563572896b3208455
0132997 <0132997@kdu-online.com>
2022-02-12 14:22:06 +0800

    Game Over
    Note: Abit confuse with this 1

---
+++ Assets/2_Scripts/UI/GameOverScreen.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public void ReviveButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
