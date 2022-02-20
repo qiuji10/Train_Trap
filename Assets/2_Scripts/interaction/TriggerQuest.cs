b870b41aab20250451169e31312d1750fb4f4321
qiuji <74701224+qiuji10@users.noreply.github.com>
2022-02-21 01:38:41 +0800

    Fix bug

--- Assets/2_Scripts/interaction/TriggerQuest.cs
+++ Assets/2_Scripts/interaction/TriggerQuest.cs
@@ -26,9 +26,9 @@ public class TriggerQuest : MonoBehaviour
         if (isInRange && hasCoke && takenCola==false)
         {
             triggerQuest.Invoke();
+            Destroy(GameObject.FindGameObjectWithTag("coke"));
             if (Input.GetKeyDown(KeyCode.E))
             {
-                Destroy(GameObject.FindGameObjectWithTag("Coke"));
                 Instantiate(ticketPrefab);
                 inventory.isFull[loopCount] = false;
                 PlayerCore.instance.inventoryName.RemoveAt(loopCount);
