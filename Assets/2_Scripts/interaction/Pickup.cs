7a86c8b63cc5def9ab2393c47d45b72b4ac4e794
brendanOU17 <0132735@kdu-online.com>
2022-02-20 22:55:01 +0800

    update pickup

--- Assets/2_Scripts/interaction/Pickup.cs
+++ Assets/2_Scripts/interaction/Pickup.cs
@@ -6,6 +6,7 @@ public class Pickup : MonoBehaviour
 {
     private Inventory inventory;
     public GameObject itemButton;
+    public bool isInRange;
     
 
     private void Start()
@@ -13,25 +14,49 @@ public class Pickup : MonoBehaviour
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
     }
 
+
+
+    private void Update()
+    {
+        if (isInRange == true )
+        {
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+
+                    // spawn item at the first available inventory slot ! 
+                        for (int i = 0; i < inventory.slots.Length; i++)
+                        {
+                            if (inventory.isFull[i] == false)
+                            { // check whether the slot is EMPTY
+                                PlayerCore.instance.inventoryName[i] = gameObject.tag;
+                                inventory.isFull[i] = true; // makes sure that the slot is now considered FULL
+                                Instantiate(itemButton, inventory.slots[i].transform, false); // spawn the button so that the player can interact with it
+                        
+                                Destroy(gameObject);
+                                break;
+                            }
+                        }
+         
+            }
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
-            // spawn item at the first available inventory slot ! 
-                for (int i = 0; i < inventory.slots.Length; i++)
-                {
-                    if (inventory.isFull[i] == false)
-                    { // check whether the slot is EMPTY
-                        PlayerCore.instance.inventoryName[i] = gameObject.tag;
-                        inventory.isFull[i] = true; // makes sure that the slot is now considered FULL
-                        Instantiate(itemButton, inventory.slots[i].transform, false); // spawn the button so that the player can interact with it
-                        
-                        Destroy(gameObject);
-                        break;
-                    }
-                }
-         }
-     }
+            isInRange = true;
+            Debug.Log("Player is in Range");
+        }
+    }
 
- }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+            Debug.Log("Player is not in Range");
+        }
+    }
+}
 
