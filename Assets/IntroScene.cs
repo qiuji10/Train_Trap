3ac6fcaed04d1878721a0d2d57f3ff57d95c19e8
0132997 <0132997@kdu-online.com>
2022-02-27 01:35:28 +0800

    Update skip in OpeningScene
    Press Space

---
+++ Assets/IntroScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScene : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+}
