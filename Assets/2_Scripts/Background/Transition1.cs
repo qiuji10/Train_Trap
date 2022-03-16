651c2110ae921a897618229aece47969da5de92b
brendanou <0132735@kdu-online.com>
2022-03-17 01:26:14 +0800

    Update Transition1.cs

--- Assets/2_Scripts/Background/Transition1.cs
+++ Assets/2_Scripts/Background/Transition1.cs
@@ -6,18 +6,30 @@ public class Transition1 : MonoBehaviour
 {
     public Transform target;
     public float speed;
-
+    public float delay;
+    public Timer timer;
+    public float time;
+    
     
     void Start()
     {
-        
+        delay = 170;  
     }
 
     // Update is called once per frame
     void Update()
     {
+
+       time = timer.timeValue;
+
+        if (delay > time  ) 
+        { 
+
         Vector3 a = transform.position;
         Vector3 b = target.position;
         transform.position = Vector3.MoveTowards(a, b, speed);
+
+
+        }
     }
 }
