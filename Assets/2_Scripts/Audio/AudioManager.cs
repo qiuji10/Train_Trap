26a4ce9b82b1a4d8a46e0fe94b8bd6c80975dd7f
0132997 <0132997@kdu-online.com>
2022-03-20 18:08:06 +0800

    BGM and timid guy dialogue

--- Assets/2_Scripts/Audio/AudioManager.cs
+++ Assets/2_Scripts/Audio/AudioManager.cs
@@ -41,4 +41,6 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+
+
 }
---
+++ Assets/TimidGuyAHHH.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimidGuyAHHH : MonoBehaviour
+{
+    private DialogueManager dm;
+
+    private void Start()
+    {
+        dm = GameObject.Find("DialogueManager").GetComponent<DialogueManager>();
+
+        dm.nameText.text = "TimidGuy";
+        dm.dialogueText.text = "AHHHHHHH !!!";
+        StartRun();
+        EndRun();
+        return;
+    }
+   
+    public void StartRun()
+    {
+        StartCoroutine(SetDialogueActive());
+    }
+    public void EndRun()
+    {
+        StartCoroutine(SetDialogueInactive());
+    }
+
+    IEnumerator SetDialogueActive()
+    {
+        yield return new WaitForSeconds(10f);
+        dm.dialogueBox.SetActive(true);
+        GameObject.Find("Player").GetComponent<PlayerController>().enabled = false;
+
+    }
+    IEnumerator SetDialogueInactive()
+    {
+        yield return new WaitForSeconds(18f);
+        dm.dialogueBox.SetActive(false);
+        GameObject.Find("Player").GetComponent<PlayerController>().enabled = true;
+
+    }
+}
